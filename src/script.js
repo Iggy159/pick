ba26c9ba6c3e1f6ab1a3b51580add4526de48c17
Iggy159 <georgebazhenov27@gmail.com>
2022-05-21 09:55:55 +0300

    particles

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+import { ShaderPass } from "./../node_modules/three/examples/jsm/postprocessing/ShaderPass";
+
 import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
-import { RGBShiftShader } from './jsm/shaders/RGBShiftShader.js';
-import { DotScreenShader } from './jsm/shaders/DotScreenShader.js';
+import { RGBShiftShader } from "./../node_modules/three/examples/jsm/shaders/RGBShiftShader";
+
 import * as dat from 'dat.gui'
 import GLTFLoader from 'three-gltf-loader';
 import gsap from 'gsap'
@@ -42,17 +45,39 @@ gltfLoader.load('scene.gltf', (gltf) => {
   gui.add(gltf.scene.rotation, 'z').min(0).max(9)
 })
 
+const geometry = new THREE.BufferGeometry();
+const vertices = []
+
+const sprite = new THREE.TextureLoader().load( 'old.png' )
+
+for ( let i = 0; i < 10000; i ++ ) {
+
+					const x = 2000 * Math.random() - 1000;
+					const y = 2000 * Math.random() - 1000;
+					const z = 2000 * Math.random() - 1000;
+
+					vertices.push( x, y, z );
+}
+
+geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+const material = new THREE.PointsMaterial( { size: 35, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
+material.color.setHSL( 1.0, 0.3, 0.7 );
+
+const particles = new THREE.Points( geometry, material );
+scene.add( particles );
+
 // Lights
 
 // Ambient light
-	ambientLight = new THREE.AmbientLight(0xff0000, 0.015);
+	ambientLight = new THREE.AmbientLight(0xff0000, 0.051);
 	scene.add(ambientLight);
 
 	// Point light
-	pointLight = new THREE.PointLight(0xffffff, 0.55);
-	pointLight.position.set(0, 0, 0);
+	pointLight = new THREE.PointLight(0x99ccff, 0.5);
+	pointLight.position.set(40, 40, 40);
 	pointLight.castShadow = true;
-	pointLight.shadow.bias = 0.1;
+	pointLight.shadow.bias = 5.1;
 	pointLight.mapSizeWidth = 2048; // Shadow Quality
 	pointLight.mapSizeHeight = 2048; // Shadow Quality
 	scene.add(pointLight);
@@ -85,8 +110,8 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = -1.5
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 10)
+camera.position.x = -1.6
 camera.position.y = 0.5
 camera.position.z = 1.6
 scene.add(camera)
@@ -124,16 +149,22 @@ const bloomPass = new UnrealBloomPass(
   0.4,
   0.85
 );
-bloomPass.exposure = 1.5
+bloomPass.exposure = 0.6
 bloomPass.threshold = 0.0;
-bloomPass.strength = 1.0; //intensity of glow
-bloomPass.radius = 1;
+bloomPass.strength = 0.5; //intensity of glow
+bloomPass.radius = 0.6;
 
 const composer = new EffectComposer(renderer);
 composer.setSize(window.innerWidth, window.innerHeight);
 composer.renderToScreen = true;
 composer.addPass(renderScene);
 composer.addPass(bloomPass)
+
+const effect2 = new ShaderPass( RGBShiftShader );
+effect2.uniforms[ 'amount' ].value = 0.0015;
+composer.addPass( effect2 );
+
+
 /**
  * Animate
  */
@@ -147,7 +178,7 @@ const tick = () =>
 
     // Update objects
 
-    //.rotation.y = .5 * elapsedTime
+    //.rotation.y += 0.5
 
     // Update Orbital Controls
     // controls.update()
